3ac189adbb69dd347f7b7821cc5be8a2a2f97496
haruki7049 <tontonkirikiri@gmail.com>
2024-06-16 08:55:27 +0900

    docs: Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # God
 want to try to 'god pull'...?
+
+## Environments
+- Zig 0.13.0
+- Nix 2.18.2
--- flake.nix
+++ flake.nix
@@ -12,17 +12,39 @@
         overlays =
           [ (final: prev: { zigpkgs = zig.packages.${prev.system}; }) ];
         pkgs = import nixpkgs { inherit system overlays; };
+        stdenv = pkgs.stdenv;
         eachSystem = f:
           pkgs.lib.genAttrs (import systems)
           (systems: f nixpkgs.legacyPackages.${system});
         treefmtEval =
           eachSystem (pkgs: treefmt-nix.lib.evalModule pkgs ./treefmt.nix);
+        god = stdenv.mkDerivation {
+          pname = "god";
+          version = "0.1.0";
+          src = ./.;
+
+          buildInputs = with pkgs; [ zigpkgs."0.13.0" ];
+
+          buildPhase = ''
+            zig build --release=safe
+          '';
+
+          installPhase = ''
+            mkdir -p $out/bin
+            install -D544 zig-out/bin/God $out/bin
+          '';
+        };
       in {
         # Use `nix fmt`
         formatter = treefmtEval.${system}.config.build.wrapper;
 
         # Use `nix flake check`
         checks.formatting = treefmtEval.${system}.config.build.check self;
+
+        packages = {
+          inherit god;
+          default = god;
+        };
 
         devShells.default = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [ zigpkgs."0.13.0" ];
